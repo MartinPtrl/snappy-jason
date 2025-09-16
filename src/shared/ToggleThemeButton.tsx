396a5ae19c2018d86547b23342585b0e8456086c
Martin Petrla <martin.petrla@gmail.com>
2025-09-16 12:44:59 +0200

    extract theme toggle to separate component

---
+++ src/shared/ToggleThemeButton.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect, useCallback } from "react";
+
+// Detect OS theme preference
+const getOSTheme = (): "light" | "dark" => {
+  if (typeof window !== "undefined" && window.matchMedia) {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  }
+  return "dark"; // fallback
+};
+
+export function ToggleThemeButton() {
+  const [theme, setTheme] = useState<"light" | "dark">(getOSTheme);
+
+  const toggleTheme = useCallback(
+    () => setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light")),
+    []
+  );
+
+  // apply theme to body, which is used in CSS
+  useEffect(() => document.body.setAttribute("data-theme", theme), [theme]);
+
+  // Listen for OS theme changes
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.matchMedia) {
+      const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+      const handleThemeChange = (e: MediaQueryListEvent) => {
+        setTheme(e.matches ? "dark" : "light");
+      };
+
+      mediaQuery.addEventListener("change", handleThemeChange);
+
+      return () => mediaQuery.removeEventListener("change", handleThemeChange);
+    }
+  }, []);
+
+  return (
+    <button onClick={toggleTheme} className="theme-toggle-button">
+      {theme === "light" ? "🌙" : "☀️"}
+    </button>
+  );
+}
